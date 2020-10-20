b1c737513fd6c7dfb7d88d149f2af00ad9f7c3c9
isllyend <isllyend@gmail.com>
2020-10-20 09:44:57 +0800

    1.first commit 2.基础module创建 3. 登陆模块创建并可动态配置

---
+++ App/src/main/java/com/chigo/kotlinmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.chigo.kotlinmall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chigo.kotlinmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
