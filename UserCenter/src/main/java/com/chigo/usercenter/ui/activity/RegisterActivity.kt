e00a8d3eef2bbf41f87838ae1c27660ed11c48e7
isllyend <isllyend@gmail.com>
2020-11-02 20:57:27 +0800

    ready for next step

--- UserCenter/src/main/java/com/chigo/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/chigo/usercenter/ui/activity/RegisterActivity.kt
@@ -1,13 +1,19 @@
 package com.chigo.usercenter.ui.activity
 
+import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.chigo.usercenter.R
-
+import kotlinx.android.synthetic.main.activity_register.*
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegisterBtn.setOnClickListener {
+            Toast.makeText(this,"register",Toast.LENGTH_SHORT).show();
+        }
     }
 }
